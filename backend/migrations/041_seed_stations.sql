054bbfcbc6270e69f9af56645aa1533b67918fb6
ShonFerns004 <shawnferns004@gmail.com>
2026-06-25 14:32:17 +0530

    update seed script: support multiple NGO UUIDs, skip existing

--- backend/migrations/041_seed_stations.sql
+++ backend/migrations/041_seed_stations.sql
@@ -5,11 +5,19 @@
 
 DO $$
 DECLARE
-  ngo_id_val UUID := 'REPLACE_WITH_YOUR_NGO_UUID';  -- <-- CHANGE THIS to your NGO's UUID
+  ngo_ids UUID[] := ARRAY['REPLACE_WITH_UUID_1'::UUID, 'REPLACE_WITH_UUID_2'::UUID];  -- <-- list all your NGO UUIDs here
+  ngo_id UUID;
   i INTEGER;
+  existing_count INTEGER;
 BEGIN
-  FOR i IN 1..37 LOOP
-    INSERT INTO fro_station_assignments (ngo_id, station, assigned_by)
-    VALUES (ngo_id_val, 'new_ucs-' || i, NULL);
+  FOREACH ngo_id IN ARRAY ngo_ids LOOP
+    FOR i IN 1..37 LOOP
+      SELECT COUNT(*) INTO existing_count FROM fro_station_assignments
+        WHERE ngo_id = ngo_id AND station = 'new_ucs-' || i;
+      IF existing_count = 0 THEN
+        INSERT INTO fro_station_assignments (ngo_id, station, assigned_by)
+        VALUES (ngo_id, 'new_ucs-' || i, NULL);
+      END IF;
+    END LOOP;
   END LOOP;
 END $$;
